88358fdc1a0f7d0eb28faccfd3675060cb0680c9
sergiomirazo <sergiomisael91@gmail.com>
2024-01-07 10:30:19 -0700

    portada, form

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'ignaciorg710@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'estancia_sanignacio@outlook.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulario de contacto:  $name";
 $email_body = "Has recibido un mensaje del formulario de contacto.\n\n"."Aquí los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nCel: $phone\n\nMensaje:\n$message";
 $headers = "From: noreply@estancia-geriatrica.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
